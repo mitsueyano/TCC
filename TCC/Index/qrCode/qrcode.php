bb3e4d103c0f5f5037dd50399737493c366c1eac
Luiza <mitsueyanoo@gmail.com>
2023-10-18 16:00:57 -0300

    Adicionado script de geração de QRCode

---
+++ TCC/Index/qrCode/qrcode.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QR CODE</title>
+    <script type="text/javascript" src="qrcode.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
